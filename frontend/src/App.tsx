b3d32f4eb9a5333c16513ba890952341bda5f68f
Victor Klomp <victor@gewis.nl>
2025-03-25 23:18:34 +0100

    add switching between team and user, and stats

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,23 @@ import './App.css'
 import {useEffect, useState} from "react";
 import {CircleMarker, Polyline, Popup, Tooltip} from "react-leaflet";
 import uniqolor from "uniqolor";
+import { Panel } from 'primereact/panel';
+import {InputSwitch} from "primereact/inputswitch";
+import distance from "@turf/distance"
 
 interface Datapoint {
+    team: string;
     user: string;
     teamName: string;
     latitude: number;
     longitude: number;
     battery: number;
     time: Date;
+    distanceLeft: number;
+}
+
+function getColor(name: string) {
+    return uniqolor(name, { lightness: [35, 50] }).color
 }
 
 function App() {
@@ -19,18 +28,22 @@ function App() {
 
     const [groupedDatapoints, setGroupedDatapoints] = useState<Record<string, Datapoint[]>>({});
 
+    const [groupByTeam, setGroupByTeam] = useState<boolean>(true);
+
     function getTracks() {
         fetch("/api/tracks")
             .then(res => res.json())
             .then(data => {
                 setDatapoints(data.map((d: any) => {
                     return {
+                        team: d.team,
                         user: d.user,
                         teamName: d.user,
                         latitude: d.latitude,
                         longitude: d.longitude,
                         battery: d.battery,
-                        time: new Date(d.timestamp*1000)
+                        time: new Date(d.timestamp*1000),
+                        distanceLeft: distance([d.longitude, d.latitude] /*, [5.4762087, 51.4435241] */ , [10.003503, 53.5655278] )
                     }
                 }))
             })
@@ -45,42 +58,87 @@ function App() {
     }, [])
 
     useEffect(() => {
-        const uniqueTeams = new Set<string>();
+        const uniqueEntities = new Set<string>();
 
         for (const point of datapoints) {
-            uniqueTeams.add(point.user);
+            if (groupByTeam) {
+                uniqueEntities.add(point.team);
+            } else {
+                uniqueEntities.add(point.user);
+            }
         }
 
         const _groupedDatapoints: Record<string, Datapoint[]> = {};
 
-        for (const uniqueTeam of uniqueTeams) {
-            _groupedDatapoints[uniqueTeam] = datapoints.filter(d => d.user === uniqueTeam);
+        for (const uniqueEntity of uniqueEntities) {
+            _groupedDatapoints[uniqueEntity] = datapoints.filter(d => {
+                return groupByTeam && d.team === uniqueEntity
+                    || d.user === uniqueEntity
+            });
         }
 
-        console.log(_groupedDatapoints);
         setGroupedDatapoints(_groupedDatapoints);
-    }, [datapoints])
+    }, [datapoints, groupByTeam])
 
     return (
         <>
-            <MapContainer className={"map-container"} center={[52.278,7.973]} zoom={8}>
+            { /* Overlay */ }
+            <div className={"absolute flex flex-row justify-end w-full"}>
+                <div className={"flex flex-col w-2xs"}>
+                    <Panel header={"Controls"} className={"bg-white flex flex-col m-1 z-500 bg rounded"} toggleable collapsed={true}>
+                        <div className={"flex flex-row"}>
+                            <InputSwitch
+                                checked={!groupByTeam}
+                                onChange={(e) => setGroupByTeam(!e.value)}
+                            />
+                            <div className={"ml-2"}>Individual view</div>
+                        </div>
+                    </Panel>
+                    <Panel header={"Stats"} className={"bg-white flex flex-col mx-1 z-500 bg rounded"} toggleable collapsed={true}>
+                        <div className={"grid grid-cols-2 justify-items-start"}>
+                            { Object.values(groupedDatapoints)
+                                .sort((a, b) => {
+                                    return a[a.length-1].distanceLeft - b[b.length-1].distanceLeft
+                                })
+                                .map((p) => {
+                                    p.sort((a, b) => a.time.getTime()-b.time.getTime())
+                                    const lastPos = p[p.length-1]
+                                    const color = getColor(groupByTeam ? lastPos.team : lastPos.user)
+                                    return <>
+                                        <div style={{ color }}>
+                                            { groupByTeam ? lastPos.team : lastPos.user }
+                                        </div>
+                                        <div>
+                                            { Math.round(lastPos.distanceLeft) } km left
+                                        </div>
+                                    </>
+                                })
+                            }
+                        </div>
+                    </Panel>
+                </div>
+            </div>
+            <MapContainer className={"map-container z-10"} center={[52.278,7.973]} zoom={8}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 {
                     Object.values(groupedDatapoints).map((p) => {
+                        p.sort((a, b) => a.time.getTime()-b.time.getTime())
                         const lastPos = p[p.length-1]
-                        const color = uniqolor(lastPos.user, { lightness: 45 }).color
+                        const color = getColor(groupByTeam ? lastPos.team : lastPos.user)
+                        console.log(lastPos);
                         return <>
                             <CircleMarker center={[lastPos.latitude, lastPos.longitude]} pathOptions={{ color }} radius={10}>
                                 <Popup>
                                     <b>Last position details:</b> <br/>
+                                    Time: { lastPos.time.toLocaleString() } <br/>
+                                    Reported by: { lastPos.user } <br/>
                                     Battery: { lastPos.battery } <br/>
-                                    Time: { lastPos.time.toLocaleString() }
                                 </Popup>
                                 <Tooltip direction="bottom" offset={[0, 10]} opacity={1} permanent>
-                                    { lastPos.user }
+                                    { groupByTeam ? lastPos.team : lastPos.user }
                                 </Tooltip>
                             </CircleMarker>
                             <Polyline
