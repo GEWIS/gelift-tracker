f244c5ffa965bf932722b1867cb7fd3b6f0e5561
Victor Klomp <victor@gewis.nl>
2026-05-13 18:28:19 +0200

    fix: debugging statement for duplicate packets

--- backend/internal/mqtt/run.go
+++ backend/internal/mqtt/run.go
@@ -103,6 +103,8 @@ func locationHandler(db *gorm.DB) func(paho.PublishReceived) (bool, error) {
 			payload.Timestamp, parts[2],
 		).First(&models.LocationPoint{}).RowsAffected
 
+		fmt.Printf("Received packet from %s / %s, on timestamp %d. Found %d in the database.\n", parts[1], parts[2], payload.Timestamp, amount)
+
 		if amount >= 1 {
 			fmt.Printf("Duplicate packet received from %s / %s, discarding...\n", parts[1], parts[2])
 			return true, nil
