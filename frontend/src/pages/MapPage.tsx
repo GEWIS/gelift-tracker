1f49837c265b263bf5b5eb1832c8ada1c988d803
Victor Klomp <victor@gewis.nl>
2026-05-13 23:19:34 +0200

    feat: switch to maptiler map

--- frontend/src/pages/MapPage.tsx
+++ frontend/src/pages/MapPage.tsx
@@ -289,7 +289,7 @@ export function MapPage() {
             <MapContainer className={"map-container z-10"} center={[50.624,5.516]} zoom={7}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    url="https://api.maptiler.com/maps/streets-v4/{z}/{x}/{y}.png?key=qhwIsthq0Hy5WSSM4PJJ"
                 />
                 {
                     Object.values(groupedDatapoints).map((p) => {
